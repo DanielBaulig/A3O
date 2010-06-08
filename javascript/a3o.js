4f0d2b860a96a51cb1e9212a61dba5d032f1030e
Daniel Baulig <daniel.baulig@gmx.de>
2010-06-09 01:50:27 +0200

    some cleanup

---
+++ javascript/a3o.js
@@ -0,0 +1,43 @@
+function error(message)
+{
+	alert(message);
+}
+
+function drawPolygon(canvasContext, vertices, fillColor = false)
+{
+	if (vertices.length() > 0)
+	{
+		canvasContext.strokeStyle = 'black';
+		if (fillColor)
+		{
+			canvasContext.fillStyle = fillColor;
+		}
+		
+		canvasContext.beginPath();
+		if (vertices[0].length() == 2)
+		{
+			canvasContext.moveTo(vertices[0][0], vertices[0][1]);
+		}
+		else
+		{
+			error('Cannot draw polygon: vertex does not have exactly 2 components!');
+		}
+		for(var i = 1; i < vertices.length(); i++)
+		{
+			if (vertices[i].length() == 2)
+			{
+				canvasContext.moveTo(vertices[i][0], vertices[i][1]);
+			}
+			else
+			{
+				error('Cannot draw polygon: vertex does not have exactly 2 components!');
+			}
+		}
+		canvasContext.endPath();
+		if (fillColor)
+		{
+			canvasContext.fill();
+		}
+		canvasContext.stroke();
+	}
+}
