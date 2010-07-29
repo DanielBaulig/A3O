cd2e0284324049ad0d7433fd197cfe43e4597e97
DanielBaulig <danie.baulig@gmx.de>
2010-07-29 10:08:43 +0200

    A3GameZone improvements

--- include/classes/GameState/A3GameZone.php
+++ include/classes/GameState/A3GameZone.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__FILE__).'/../Registry.php';
+
 abstract class A3GameZoneRegistry
 {
 	private static $instance = null;
@@ -22,6 +24,10 @@ abstract class A3GameZoneRegistry
 		}
 		return self::$instance;
 	}
+	public static function getElement( $key )
+	{
+		return self::getInstance( )->getElement( $key );
+	}
 }
 
 class A3GameZone
@@ -29,23 +35,16 @@ class A3GameZone
 	protected $m_name;	
 	protected $m_connections = array( );
 	/** Associative Array of Pieces (Units)
-	 * Piecetype => Remaining Movement => Count
+	 * Nation => Piecetype => Remaining Movement => Count
 	 * 
-	 * eg $this->pieces['infantry'][1] = 10;
+	 * eg $this->m_pieces['russia']['infantry'][1] = 10;
 	 * -> 10 pieces of infantry that have 1 remaining movement
 	 * if 3 move 1 field from this to other
-	 * -> $this->pieces['infantry'][1] -= 3;
-	 * -> $other->pieces['infantry'][0] += 3;
+	 * -> $this->m_pieces['russia']['infantry'][1] -= 3;
+	 * -> $other->m_pieces['russia']['infantry'][0] += 3;
 	 * 
-	 * I should implement something like piecesCount( $type ), eg.
-	 * 
-	 * for($i = 0; $i <= getTypeAttributes( $type )->maximumMovement; $i++)
-	 * {
-	 * 		$sum += $this->pieces[$type][$i];
-	 * }
-	 * return $sum;
-	 * 
-	 * @var unknown_type
+	 * @var array $pieces
+	 * @access protected
 	 */
 	protected $m_pieces = array( );
 	protected $m_owner = null;
@@ -55,28 +54,79 @@ class A3GameZone
 	/** Returns the number of pieces of the given type
 	 * 
 	 * @param string $type
+	 * @return int 
 	 */
-	public function countPieces( $type, $minimumRemainingMovement = 0 )
+	public function countPieces( $nation, $type, $minimumRemainingMovement = 0 )
 	{
 		$piecesCount = 0;
-		if ( array_key_exists( $this->m_pieces, $type ) )
+		if ( array_key_exists( $this->m_pieces, $nation ) && array_key_exists( $this->m_pieces[$nation], $type ) )
 		{
-			for( $i=$minimumRemainingMovement; $i <= getTypeAttributes( $type )->maximumMovement; $i++ )
+			for( $i=$minimumRemainingMovement; $i <= A3GameTypeRegistry::getElement( $type )->maximumMovement; $i++ )
 			{
-				$piecesCount += $this->m_pieces[$type][$i];
+				$piecesCount += $this->m_pieces[$nation][$type][$i];
 			}
 		}
 		return $piecesCount;
 	}
 	
-	public function canMovePieces( $count, $type, $distance )
+	/** Moves up to $count pieces of nation $nation and type $type to zone $target draining $distance movement.
+	 * 
+	 * Returns true if transfer was successfull and false if there was a problem (eg. 
+	 * less pieces available than specified or target zone not valid).
+	 * Even if returning false the method will transfer as many pieces as possible.
+	 * Use {@link canMovePieces} and {@link isPathValid} to check if a given move is valid 
+	 * entirely without moving any pieces.
+	 * 
+	 * @param int $count
+	 * @param string $nation
+	 * @param string $type
+	 * @param int $distance
+	 * @param string $target
+	 * @return boolean
+	 */
+	public function movePieces( $count, $nation, $type, $distance, $target )
 	{
-		return countPieces( $type, $distance ) >= $count;
+		if ( array_key_exists( $this->m_pieces, $nation ) && array_key_exists( $this->m_pieces[$nation], $type ) )
+		{
+			$total = 0;
+			$target = A3GameZoneRegistry::getElement( $target );
+			if ( $target !== null )
+			{
+				for( $i = $distance; $i <= A3GameTypeRegistry::getElement( $type )->maximumMovement; $i++ )
+				{
+					$moved = $count > $this->m_pieces[$nation][$type][$i] ? $this->m_pieces[$nation][$type][$i] : $count;
+					$count = $count - $moved;
+					$target->m_pieces[$nation][$type][$i - $distance] += $moved;
+					$this->m_pieces[$nation][$type][$i] -= $moved;
+					$total += $moved;
+				}
+				if ( $total === $count )
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/** Returns if the amount of pieces of $nation nation and $type type can be moved 
+	 * (transfered) from this zone to the given zone.
+	 * 
+	 * @param int $count
+	 * @param string $nation
+	 * @param string $type
+	 * @param int $distance
+	 * @return boolean
+	 */
+	public function canMovePieces( $count, $nation, $type, $distance )
+	{
+		return $this->countPieces( $nation, $type, $distance ) >= $count;
 	}
 	
 	/** Checks if this zone has a connection to the given zone.
 	 * 
 	 * @param string $zone
+	 * @return boolean
 	 */
 	public function hasConnection( $zone )
 	{
@@ -91,28 +141,55 @@ class A3GameZone
 	 * specified or set to null, isValidPath will allow
 	 * water aswell as land zones on the path.
 	 * 
+	 * The optional parameter $alliance specifies if the
+	 * zones are checked against ownership and only a path
+	 * controlled by the specified alliance may be traversed.
+	 * 
+	 * The optional parameter $combat speciyfies if the
+	 * movement may end in an enemy zone, thus triggering
+	 * combat. Defaults to false.
+	 * 
 	 * @param array $path
 	 * @param boolean $water (optional)
+	 * @param string $alliance (optional)
+	 * @param boolean $combat (optional)
 	 * @return boolean
 	 */
-	public function isValidPath( array $path, $water = null )
+	public function isValidPath( array $path, $water = null, $alliance = null, $combat = false )
 	{
 		$zone = $this;
+		$alliance = A3GameAllianceRegistry::getElement( $alliance );
+		
+		// get zone where movement ends
+		$endZone = end( $path );
+		
 		foreach( $path as $step )
 		{
+			// dont allow to enter a zone that is not connected
 			if ( !$zone->hasConnection( $step ) )
 			{
 				return false;
 			}
+			
+			// create/get A3GameZone object for the upcoming zone
+			$zone = A3GameZoneRegistry::getElement( $step );
+			
+			// if water is null (no terrain checking) simply skip
+			// if water is true, only allow to enter water
+			// if water is false, only allow to enter land
 			if ( $water !== null && $water !== $zone->m_water )
 			{
 				return false;
 			}
-			$zone = A3GameZoneRegistry::getInstance( )->getElement( $step );
-		}
-		if ( $water !== null && $water !== $zone->m_water )
-		{
-			return false;
+			// if alliance checking is enabled only allow to enter zones controlled by given alliance
+			if( $alliance !== null && ! $alliance->isAllied( $zone->m_owner ) )
+			{
+				// however only, if this is not the last zone and triggering combat is not allowed
+				if ( !($step === $endZone && $combat) )
+				{
+					return false;
+				}				
+			}
 		}
 		return true;
 	}
--- include/classes/Registry.php
+++ include/classes/Registry.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Logger.php';
+require_once dirname(__FILE__).'/Logger.php';
 
 interface IFactory
 {
