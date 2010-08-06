43c6e3c55a9eb1a10cf5dedc186c8d42e65152a8
Daniel Baulig <daniel.baulig@gmx.de>
2010-08-06 17:24:20 +0200

    Added an additional Layer of abstraction - or evened the abstraction level ;)

---
+++ include/classes/MatchState/A3/A3MatchPlayer.php
@@ -0,0 +1,66 @@
+<?php
+
+class A3MatchPlayerPDOFactory extends MatchPlayerPDOFactory
+{
+	protected function createObject( $data )
+	{
+		return new A3MatchPlayer( $this->m_match, $data );
+	}
+}
+
+class A3MatchPlayer extends MatchPlayer
+{
+	public function researchJetFighters( )
+	{
+		$this->setOption( 'technology_jetfighters' , 1 );
+	}
+	
+	public function researchSuperSubs( )
+	{
+		$this->setOption( 'technology_supersubs', 1 );
+	}
+	
+	public function researchRockets( )
+	{
+		$this->setOption( 'technology_rockets', 1 );
+	}
+	public function researchLongRangeAircraft( )
+	{
+		$this->setOption( 'technology_longrangeaircraft', 1 );
+	}
+	
+	public function researchCombinedBombardment( )
+	{
+		$this->setOption( 'technology_combinedbombardment', 1 );
+	}
+	
+	public function researchHeavyBombers( )
+	{
+		$this->setOption( 'technology_heavybombers', 1 );
+	}
+	
+	public function hasJetFightersResearched( )
+	{
+		return $this->getOption( 'technology_jetfighters' );
+	}
+	
+	public function hasSuperSubsResearched( )
+	{
+		return $this->getOption( 'technology_supersubs' );
+	}
+	
+	public function hasRocketsResearched( )
+	{
+		return $this->getOption( 'technology_rockets' );
+	}
+	
+	public function hasLongRangeAircraftResearched( )
+	{
+		return $this->getOption( 'technlogy_longrangeaircraft');
+	}
+	
+	public function hasCombinedBombardmentResearched( )
+	{
+		return $this->getOption( 'technology_combinedbombardment' );
+	}
+}
