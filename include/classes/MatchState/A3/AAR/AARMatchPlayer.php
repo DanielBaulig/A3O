5602807a174be0dec745508853a4d1786d122808
Daniel Baulig <daniel.baulig@gmx.de>
2010-08-06 17:35:38 +0200

    Accidently implmented Tech research options in A3 set not AAR (not all Axis variants support techs)

--- include/classes/MatchState/A3/AAR/AARMatchPlayer.php
+++ include/classes/MatchState/A3/AAR/AARMatchPlayer.php
@@ -8,5 +8,58 @@ class AARMatchPlayerPDOFactory extends A3MatchPlayerPDOFactory
 }
 
 class AARMatchPlayer extends A3MatchPlayer
-{	
+{
+public function researchJetFighters( )
+	{
+		$this->setOption( 'technology_jetfighters' , 1 );
+	}
+	
+	public function researchSuperSubs( )
+	{
+		$this->setOption( 'technology_supersubs', 1 );
+	}
+	
+	public function researchRockets( )
+	{
+		$this->setOption( 'technology_rockets', 1 );
+	}
+	public function researchLongRangeAircraft( )
+	{
+		$this->setOption( 'technology_longrangeaircraft', 1 );
+	}
+	
+	public function researchCombinedBombardment( )
+	{
+		$this->setOption( 'technology_combinedbombardment', 1 );
+	}
+	
+	public function researchHeavyBombers( )
+	{
+		$this->setOption( 'technology_heavybombers', 1 );
+	}
+	
+	public function hasJetFightersResearched( )
+	{
+		return $this->getOption( 'technology_jetfighters' );
+	}
+	
+	public function hasSuperSubsResearched( )
+	{
+		return $this->getOption( 'technology_supersubs' );
+	}
+	
+	public function hasRocketsResearched( )
+	{
+		return $this->getOption( 'technology_rockets' );
+	}
+	
+	public function hasLongRangeAircraftResearched( )
+	{
+		return $this->getOption( 'technlogy_longrangeaircraft');
+	}
+	
+	public function hasCombinedBombardmentResearched( )
+	{
+		return $this->getOption( 'technology_combinedbombardment' );
+	}	
 }
