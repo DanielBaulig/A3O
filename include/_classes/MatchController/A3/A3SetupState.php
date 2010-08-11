128cf39120f52c6ef61b81e2854c21aaf6101303
Daniel Baulig <daniel.baulig@gmx.de>
2010-08-11 19:04:08 +0200

    Remove 'Ghost Folders' - once again

--- include/_classes/MatchController/A3/A3SetupState.php
+++
@@ -1,4 +0,0 @@
-<?php
-class A3SetupState extends UnimplementedState
-{
-}
--- include/_classes/MatchController/MatchController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once dirname( __FILE__ ) . '/IState.php';
-require_once dirname( __FILE__ ) . '/Action.php';
-require_once dirname( __FILE__ ) . '/MatchContext.php';
-require_once dirname( __FILE__ ) . '/TurnSequence.php';
-require_once dirname( __FILE__ ) . '/A3/A3States.php';
