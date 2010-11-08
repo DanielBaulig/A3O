4543762bdf73fa2360228255a974052f0fefe1df
Daniel Baulig <daniel.baulig@gmx.de>
2010-11-08 22:52:45 +0100

    Replaced that anyoing "Insert title here" with something meaningfull

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Insert title here</title>
+	<title>A3O - a browser-based TripleA clone using modern web-technlogies.</title>
 	<script type="text/javascript" src="static/javascript/jquery-1.4.3.min.js"></script>
 	<script type="text/javascript" src="static/javascript/jquery.mousewheel.min.js"></script>
 	<script type="text/javascript" src="static/javascript/rtree.js"></script>
